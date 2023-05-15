defa40aaffbe08fac277389e53838b4919503d7e
John Poikilidis <akiskawi@gmail.com>
2023-05-15 11:33:41 +0300

    core done

--- src/main/java/com/booleanuk/core/Computer.java
+++ src/main/java/com/booleanuk/core/Computer.java
@@ -4,20 +4,28 @@ import java.util.ArrayList;
 
 public class Computer {
     public ArrayList<Game> installedGames = new ArrayList<>();
+    private PowerSupply psu;
+
+    public Computer(PowerSupply psu) {
+        this.psu = psu;
+    }
+
+    public Computer( PowerSupply psu,ArrayList<Game> installedGames) {
+        this.installedGames = installedGames;
+        this.psu = psu;
+    }
 
     public void turnOn() {
-        PowerSupply psu = new PowerSupply();
         psu.turnOn();
     }
 
-    public void installGame() {
-        Game game = new Game("Morrowind");
-        this.installedGames.add(game);
+    public void installGame(String gameName) {
+        this.installedGames.add(new Game(gameName));
     }
 
-    public String playGame() {
+    public String playGame(String gameName) {
         for (Game g : this.installedGames) {
-            if (g.name.equals("Morrowind")) {
+            if (g.name.equals(gameName)) {
                 return g.start();
             }
         }
